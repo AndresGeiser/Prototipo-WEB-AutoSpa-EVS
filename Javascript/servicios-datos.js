6560bc0a891d3c0a23f620e6b22ed16a01d7765c
AndresGeiser <geiserandres00@gmail.com>
2020-06-18 21:45:19 -0300

    - Primera carga de servicios.

---
+++ Javascript/servicios-datos.js
@@ -0,0 +1,60 @@
+var servicios = [
+    {
+        id : 1,
+        nombre : "Tratamiento acrílico",
+        descripcion: "" ,
+        precio: "$ 0",
+       
+    },
+    {
+        id : 2,
+        nombre : "Tratamiento cerámico",
+        descripcion: "" ,
+        precio: "$ 0",
+
+       
+    },
+    {
+        id : 3,
+        nombre : "Pulido y sellado de ópticas",
+        descripcion: "" ,
+        precio: "$ 0",
+
+    },
+    {
+        id : 4,
+        nombre : "Sellado de cristales y repelente para la lluvia",
+        descripcion: "" ,
+        precio: "$ 0",
+
+    },
+    {
+        id : 5,
+        nombre : "Pulido y sellado de escape y cromados",
+        descripcion: "" ,
+        precio: "$ 0",
+
+    },
+    {
+        id : 6,
+        nombre : "Limpieza y sellado de llantas",
+        descripcion: "" ,
+        precio: "$ 0",
+
+    },
+    {
+        id : 7,
+        nombre : "Limpieza interior y tapicería de cuero",
+        descripcion: "" ,
+        precio: "$ 0",
+
+    },
+    {
+        id : 8,
+        nombre : "Limpieza de motor",
+        descripcion: "" ,
+        precio: "$ 0",
+
+    },
+
+]
---
+++ Javascript/servicios.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    
+  
+    cargarServicios(servicios)
+});
+
+function cargarServicios(servicios) {
+
+    var publicaciones = document.querySelector('.servicios');
+
+    for (let servicio of servicios) {
+
+        publicaciones.innerHTML +=` 
+        <div class="servicio" id="${servicio.id}" >
+        <div class="datos">
+            <h3>${servicio.nombre}</h3>
+            <h4>${servicio.precio}</h4>
+        </div>
+        <div>
+            <button onclick="mostrarDetalles(this)">Ver mas</button>
+        </div>
+        </div>
+        `    
+    }  
+}
+
+
+function mostrarDetalles(seleccionado) {
+
+    var foto =  document.querySelector('.cont_ventana .ventana figure img');
+    var info =  document.querySelector('.cont_ventana .ventana .info');
+
+    servicios.forEach(servicio => {
+
+        var aviso = seleccionado.parentElement.parentElement; //Nos devuelve el elemento que contiene al aviso 
+
+        if(servicio.id == aviso.id) {
+        
+            info.innerHTML = `
+            <p><b>Nombre:</b> ${servicio.nombre}.</p><br>
+            <p><b>Precio:</b> ${servicio.precio}.</p><br>
+            <p><b>Descripción:</b> ${servicio.descripcion}.</p><br>
+            `;
+        }
+     });
+ 
+    document.querySelector('.cont_ventana').style.visibility = "visible";   
+}
+
+function cerrarVentana() {
+    document.querySelector('.cont_ventana').style.visibility = "hidden";
+}
+
